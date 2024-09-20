14924b794d3d2a405492354373af394ab8592aa5
Илья Кузьмин <ilia.kuzmin.01.01.01.01@mail.ru>
2024-09-20 16:27:29 +0300

    Feature animation

--- src/components/NotFound/Notfound.tsx
+++ src/components/NotFound/Notfound.tsx
@@ -1,30 +1,9 @@
-import styled from "styled-components";
-
-const NotFoundContainer = styled.div`
-   position: relative;
-   top: 0;
-   left: 0;
-   width: 100%;
-   text-align: center;
-   margin-top: 20px;
-
-   img {
-      width: 65%;
-   }
-
-   p {
-      font-size: 22px;
-      font-weight: 500;
-      margin-top: 12px;
-   }
-`;
-
 const NotFound = () => {
    return (
-      <NotFoundContainer>
+      <>
          <img src="/src/images/404.png" alt="Not found" />
          <p>Oops! Location not found.</p>
-      </NotFoundContainer>
+      </>
    );
 };
 
--- src/components/WeatherWrapper/WeatherWrapper.tsx
+++ src/components/WeatherWrapper/WeatherWrapper.tsx
@@ -3,14 +3,14 @@ import styled from "styled-components";
 import Search from "../Search/Search";
 import Main from "../Main/Main";
 import Info from "../Info/Info";
+import NotFound from "../NotFound/Notfound";
 
 import { useAppSelector } from "../../types/hooks";
-import NotFound from "../NotFound/Notfound";
 
 const WeatherContainer = styled.div`
    position: relative;
    width: 400px;
-   // height: 555px;
+   height: ${({ height }) => height};
    background: rgba(255, 255, 255, 0.1);
    backdrop-filter: blur(30px);
    border: 2px solid rgba(255, 255, 255, 0.2);
@@ -20,23 +20,60 @@ const WeatherContainer = styled.div`
    transition: height 0.6s ease;
 `;
 
+const NotFoundContainer = styled.div`
+   position: relative;
+   top: 0;
+   left: 0;
+   width: 100%;
+   text-align: center;
+   margin-top: 20px;
+
+   img {
+      width: 65%;
+   }
+
+   p {
+      font-size: 22px;
+      font-weight: 500;
+      margin-top: 12px;
+   }
+`;
+
+const ContainerAnimation = styled.div`
+   transform: translateY(-100%);
+`;
+
+const ContainerAnimationActive = styled.div`
+   transform: translateY(0%);
+   transition: transform 1s ease;
+   transition-delay: "0.6s";
+`;
+
 const WeatherWrapper = (): JSX.Element => {
    const { weather, error } = useAppSelector((state) => state.weatherSlice);
 
-   let weatherContainerHeight;
-   if (weather.description) weatherContainerHeight = { height: "555px" };
-   if (error) weatherContainerHeight = { height: "400px" };
+   let weatherContainerHeight = "99px";
+   if (weather.description) weatherContainerHeight = "555px";
+   if (error) weatherContainerHeight = "400px";
 
    return (
-      <WeatherContainer style={weatherContainerHeight}>
+      <WeatherContainer height={weatherContainerHeight}>
          <Search />
          {!error && weather.description && (
             <>
-               <Main />
+               <ContainerAnimation>
+                  <Main />
+               </ContainerAnimation>
                <Info />
             </>
          )}
-         {error && <NotFound />}
+         {error && (
+            <NotFoundContainer>
+               <ContainerAnimation>
+                  <NotFound />
+               </ContainerAnimation>
+            </NotFoundContainer>
+         )}
       </WeatherContainer>
    );
 };
